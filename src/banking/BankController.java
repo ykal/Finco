539d966538b9ca783ee94ef473bc7e8626eeca71
Kasim <se.kasim.ebrahim@gmail.com>
2021-04-13 23:42:38 -0500

    Use CommandManager

--- src/banking/BankController.java
+++ src/banking/BankController.java
@@ -1,8 +1,8 @@
 package banking;
 
+import framework.controllers.CommandManager;
 import framework.controllers.Controller;
 import framework.controllers.commands.*;
-import framework.controllers.results.CurrentBalance;
 import framework.controllers.results.IResult;
 import framework.controllers.ruleengine.AbstractAssessor;
 import framework.controllers.ruleengine.IProperty;
@@ -10,20 +10,34 @@ import framework.controllers.ruleengine.Rule;
 import framework.models.account.Account;
 import framework.models.account.Entry;
 
+import java.awt.event.ActionEvent;
+
 public class BankController extends Controller {
+	private CommandManager commandManager;
+	private Bank view;
+
+	public BankController(CommandManager commandManager, Bank view) {
+		this.commandManager = commandManager;
+		this.view = view;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
+
 	@Override
 	protected void deposit(Entry entry, Account account){
-		LoggedAction deposit = new Deposit(entry, account);
+		LoggableAction deposit = new Deposit(entry, account);
 		deposit = new Proxy(deposit);
-		IResult result = deposit.execute();
+		IResult result = commandManager.submit(deposit);
 		runNotifyRule(entry, account, result);
 	}
 
 	@Override
 	protected void withdraw(Entry entry, Account account){
-		LoggedAction withdraw = new Withdraw(entry, account);
+		LoggableAction withdraw = new Withdraw(entry, account);
 		withdraw = new Proxy(withdraw);
-		IResult result = withdraw.execute();
+		IResult result = commandManager.submit(withdraw);
 		runNotifyRule(entry, account, result);
 	}
 
@@ -31,7 +45,7 @@ public class BankController extends Controller {
 		IProperty property = new NotifyProperty(entry, result,
 				account.getOwner().getCustomerType());
 		AbstractAssessor<IProperty> assessor = new NotifyAssessor(property);
-		Rule notifyRule = new Rule(assessor, new ENotify());
+		Rule notifyRule = new Rule(assessor, new ENotify(), commandManager);
 		notifyRule.applyRule();
 	}
 }
