4b94124fb61df15e51fcea8bc57bbe4574fb1e2a
Kasim <se.kasim.ebrahim@gmail.com>
2021-04-13 23:45:12 -0500

    Implement simple CommandManager

--- src/framework/controllers/CommandManager.java
+++ src/framework/controllers/CommandManager.java
@@ -1,4 +1,10 @@
 package framework.controllers;
 
+import framework.controllers.commands.AbstractAction;
+import framework.controllers.results.IResult;
+
 public class CommandManager {
+	public IResult submit(AbstractAction action) {
+		return action.execute();
+	}
 }
