c818d9cf9c7b3675faf06e7f1a8801fce1f7255f
Kasim <se.kasim.ebrahim@gmail.com>
2021-04-13 23:46:35 -0500

    Remove unnecessary code

--- src/framework/FinCo.java
+++ src/framework/FinCo.java
@@ -1,19 +1,8 @@
 package framework;
 
-import framework.controllers.CommandManager;
-import framework.controllers.Controller;
 
 public class FinCo {
-	private Controller controller;
-	private CommandManager commandManager;
-
 	public FinCo(){}
-
-	public FinCo(Controller controller, CommandManager cm) {
-		this.controller = controller;
-		this.commandManager = cm;
-		// TODO attach controller as an action listener to GUI
-	}
 
 	public void start() {
 		//TODO main app life cycle goes here.
--- src/framework/controllers/Controller.java
+++ src/framework/controllers/Controller.java
@@ -12,17 +12,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public abstract class Controller implements ActionListener {
-	private REPFile repFile;
-	private ACCFile accFile;
-	private CUSFile cusFile;
-	public Controller(){}
-
-	public Controller(REPFile repFile, ACCFile accFile, CUSFile cusFile) {
-		this.repFile = repFile;
-		this.accFile = accFile;
-		this.cusFile = cusFile;
-	}
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 //		TODO map this to deposit, addinterest, addaccount ... methods.
