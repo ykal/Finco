9d475a8bc1ef182641110fe3b8e9eaaa4f2a1920
Kasim <se.kasim.ebrahim@gmail.com>
2021-04-14 00:41:29 -0500

    Fix float double type error

--- src/framework/models/account/Entry.java
+++ src/framework/models/account/Entry.java
@@ -6,10 +6,10 @@ public class Entry implements IEntry{
 	private double amount;
 	private LocalDate date;
 
-	public Float getAmount() {
+	public double getAmount() {
 		return amount;
 	}
-	public void setAmount(Float amount) {
+	public void setAmount(double amount) {
 		this.amount = amount;
 	}
 	public LocalDate getDate() {
