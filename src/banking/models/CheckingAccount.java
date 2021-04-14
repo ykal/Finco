5a2ee3d15a5b05ea97f0463b237dfd436ffa76ac
Kasim <se.kasim.ebrahim@gmail.com>
2021-04-14 02:44:05 -0500

    Add account ID

--- src/banking/models/CheckingAccount.java
+++ src/banking/models/CheckingAccount.java
@@ -3,6 +3,10 @@ package banking.models;
 public class CheckingAccount extends BankingAccount {
 	private Double interest = 0.01;
 
+	public CheckingAccount(String accnr) {
+		setId(accnr);
+	}
+
 	public void addInterest() {
 		Double currentBalance = super.getCurrentBalance();
 		Double newBalance = currentBalance + (currentBalance*interest);
