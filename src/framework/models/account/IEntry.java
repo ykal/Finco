d5b0e86a308d0b7561c986fa588ebfb93003198d
Kasim <se.kasim.ebrahim@gmail.com>
2021-04-14 02:50:24 -0500

    Complete Entry

--- src/framework/models/account/IEntry.java
+++ src/framework/models/account/IEntry.java
@@ -1,6 +1,10 @@
 package framework.models.account;
 
+import java.time.LocalDate;
+
 public interface IEntry {
 	double getAmount();
 	void setAmount(double v);
+	LocalDate getDate();
+	void setDate(LocalDate date);
 }
