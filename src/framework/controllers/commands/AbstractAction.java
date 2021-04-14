5f461259c654c91bd452a2b49287a77b34a7bcc7
Kasim <se.kasim.ebrahim@gmail.com>
2021-04-13 22:20:30 -0500

    Fix return IResult

--- src/framework/controllers/commands/AbstractAction.java
+++ src/framework/controllers/commands/AbstractAction.java
@@ -1,5 +1,7 @@
 package framework.controllers.commands;
 
+import framework.controllers.results.IResult;
+
 public abstract class AbstractAction {
-	public abstract void execute();
+	public abstract IResult execute();
 }
