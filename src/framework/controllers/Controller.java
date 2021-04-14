a45dfd2ca17f28e2e31f74b5e135c26a17c37ee9
Kasim <se.kasim.ebrahim@gmail.com>
2021-04-13 22:21:25 -0500

    Persistent Files

--- src/framework/controllers/Controller.java
+++ src/framework/controllers/Controller.java
@@ -1,9 +1,28 @@
 package framework.controllers;
 
+import framework.controllers.commands.*;
+import framework.controllers.results.IResult;
+import framework.models.account.Account;
+import framework.models.account.Entry;
+import framework.persistence.ACCFile;
+import framework.persistence.CUSFile;
+import framework.persistence.REPFile;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public abstract class Controller implements ActionListener {
+	private REPFile repFile;
+	private ACCFile accFile;
+	private CUSFile cusFile;
+	public Controller(){}
+
+	public Controller(REPFile repFile, ACCFile accFile, CUSFile cusFile) {
+		this.repFile = repFile;
+		this.accFile = accFile;
+		this.cusFile = cusFile;
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 //		TODO map this to deposit, addinterest, addaccount ... methods.
