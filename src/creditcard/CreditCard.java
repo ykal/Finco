9950e47a425786267fd5428fd15fa7009e103dd2
Kaleab <go4ykal@gmail.com>
2021-04-14 23:39:05 -0500

    initialize credit card application with title

---
+++ src/creditcard/CreditCard.java
@@ -0,0 +1,10 @@
+package creditcard;
+
+import framework.FinCo;
+import framework.models.Data;
+
+public class CreditCard extends FinCo {
+    public CreditCard() {
+        super("Credit Card Application", new Data());
+    }
+}
